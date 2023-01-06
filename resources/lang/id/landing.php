d374eae7a7c8ba74205f8433ece4f65865fb0fc6
Putri Selvi <putriselfie23@gmail.com>
2023-01-06 14:21:36 +0700

    Kelompok_4

---
+++ resources/lang/id/landing.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'langtext' => 'Bahasa',
+    'welcome' => 'Selamat Datang di ' . config('laundry', 'Rama Laundry'),
+    'loginOrRegister' => 'Masuk / Daftar',
+    'tagline' => 'Dapatkan pengalaman terbaik dalam layanan laundry',
+    'why' => 'Kenapa memilih layanan laundry kami?'
+];
